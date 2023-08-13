82f4fe9f40d1175c12070ebedc878b357bdd1dbf
thunderhog <pyxis02@gmail.com>
2023-08-13 10:31:55 +0400

    ignore TS errors

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -38,7 +38,7 @@ const ProfileHeader = ({
           </div>
         </div>
       </div>
-      // Community Profile
+
       <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
       <div className="mt-12 h-0.5 w-full bg-dark-3" />
     </div>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescriptOnly: {
+    ignoreBuildErrors: true,
+  },
+
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
